e093fc92d867feedfbf35157f7278e331467be31
Adham Samer <48602871+AdhamSamer3301@users.noreply.github.com>
2023-02-17 22:53:09 +0200

    Initial Commit

---
+++ Shopify/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Shopify
+//
+//  Created by Adham Samer on 17/02/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ ShopifyUITests/ShopifyUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  ShopifyUITestsLaunchTests.swift
+//  ShopifyUITests
+//
+//  Created by Adham Samer on 17/02/2023.
+//
+
+import XCTest
+
+final class ShopifyUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
