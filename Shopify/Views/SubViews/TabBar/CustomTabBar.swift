4558a1340cafd0a8e8458fc2eaf6cef085a2b8ac
Adham Samer <48602871+AdhamSamer3301@users.noreply.github.com>
2023-02-23 04:13:14 +0200

    Accent Color and TabBar minor fix

--- Shopify/Views/SubViews/TabBar/CustomTabBar.swift
+++ Shopify/Views/SubViews/TabBar/CustomTabBar.swift
@@ -2,18 +2,28 @@
 //  CustomTabBar.swift
 //  Shopify
 //
-//  Created by Adham Samer on 22/02/2023.
+//  Created by Adham Samer on 23/02/2023.
 //
 
 import UIKit
 
-class CustomTabBar: UITabBar {
+class CustomTabBar: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        self.tabBar.layer.cornerRadius = 20
+        // Do any additional setup after loading the view.
+    }
+    
 
     /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
     }
     */
 
