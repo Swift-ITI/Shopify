9f7e2da2d41e1d09e3543ab1c9bf9ff95f8a54df
ZeinabMohamed <zm4816035@gmail.com>
2023-02-26 16:57:33 -0800

    fixNavigationBar

--- Shopify/ViewModels/ViewModel.swift
+++ Shopify/ViewModels/ViewModel.swift
@@ -6,3 +6,49 @@
 //
 
 import Foundation
+import UIKit
+
+//class buttonss {
+//    var viewController : UIViewController = UIViewController()
+//
+//    init(viewController: UIViewController) {
+//        self.viewController = viewController
+//    }
+//
+//func addBarButtonItems(){
+//        let fav = UIButton()
+//    fav.setImage(UIImage(systemName: "heart"), for: .normal)
+//    fav.addTarget(viewController, action: #selector(navfav), for: .touchUpInside)
+//        viewController.navigationItem.rightBarButtonItem?.tintColor = UIColor(named: "BeigeColor")
+//
+//        let cart = UIBarButtonItem(image: UIImage(systemName: "cart"), style: .plain, target: viewController, action: #selector(navcart))
+//        viewController.navigationItem.rightBarButtonItem?.tintColor = UIColor(named: "BeigeColor")
+//    viewController.navigationItem.rightBarButtonItem?.customView = fav as UIView
+//     //   viewController.navigationItem.rightBarButtonItems = [fav as UIBarButtonItem , cart]
+//
+//        let search =  UIBarButtonItem(image: UIImage(systemName: "magnifyingglass"), style: .plain, target: viewController, action: #selector(navsearch))
+//        viewController.navigationItem.leftBarButtonItem?.tintColor = UIColor(named: "BeigeColor")
+//        viewController.navigationItem.leftBarButtonItem = search
+//
+//    }
+//
+//    @objc func navfav()
+//    {
+//        let FavouriteStoryBoardd = UIStoryboard(name: "OthersSB", bundle: nil)
+//        let favobj = FavouriteStoryBoardd.instantiateViewController(withIdentifier: "favid") as! WishListVC
+//        viewController.navigationController?.pushViewController(favobj, animated: true)
+//    }
+//    @objc func navcart()
+//    {
+//        let CartStoryBoard = UIStoryboard(name: "OthersSB", bundle: nil)
+//        let cartobj =
+//        CartStoryBoard.instantiateViewController(withIdentifier: "cartid") as! CartVC
+//        viewController.navigationController?.pushViewController(cartobj, animated: true)
+//    }
+//    @objc func navsearch()
+//    {
+//        print("aa")
+////        let searchobj = self.storyboard?.instantiateViewController(withIdentifier: "search") as! SearchViewController
+////        self.navigationController?.pushViewController(searchobj, animated: true)
+//    }
+//}
