f7efe1b80950f9b04359510f33ad05d3bfa8e4b3
Michael <michaelhany101@gmail.com>
2023-03-08 07:22:52 +0200

    finish radio buttons coding

--- Shopify/Views/Payment&Order/PaymentVC.swift
+++ Shopify/Views/Payment&Order/PaymentVC.swift
@@ -12,6 +12,9 @@ class PaymentVC: UIViewController
     @IBOutlet var payPalBtn: UIButton!
     @IBOutlet var processImage: UIImageView!
     @IBOutlet var processText: UILabel!
+    
+    var shouldPay : Int?
+    var approved : Bool = true
     /*@IBOutlet weak var addressTable: UITableView!{
         didSet{
             //addressTable.delegate = self
@@ -23,38 +26,74 @@ class PaymentVC: UIViewController
             addressTable.layer.cornerRadius = 20
         }
     }*/
-    var radioBtn: SSRadioButtonsController? {
-        didSet {
+    var radioBtn: SSRadioButtonsController?
+    {
+        didSet
+        {
             radioBtn?.delegate = self
              radioBtn?.shouldLetDeSelect = true
         }
     }
 
-    override func viewDidLoad() {
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
         radioBtn = SSRadioButtonsController(buttons: payPalBtn, codBtn)
         // Do any additional setup after loading the view.
     }
 
-    override func viewWillAppear(_ animated: Bool) {
+    override func viewWillAppear(_ animated: Bool)
+    {
     }
     
-    @IBAction func proceedOrderBtn(_ sender: Any) {
-        let orderObj = self.storyboard?.instantiateViewController(withIdentifier: "orderVC") as! OrderVC
-        
-        self.navigationController?.pushViewController(orderObj, animated: true)
+    @IBAction func proceedOrderBtn(_ sender: Any)
+    {
+        if approved
+        {
+            let orderObj = self.storyboard?.instantiateViewController(withIdentifier: "orderVC") as! OrderVC
+            
+            self.navigationController?.pushViewController(orderObj, animated: true)
+        }
+        else
+        {
+            let alert = UIAlertController(title: "", message: "Sorry you can't choose this payment Type", preferredStyle: UIAlertController.Style.alert)
+            alert.addAction(UIAlertAction(title: "Ok", style: .cancel))
+            present(alert, animated: true, completion: nil)
+        }
     }
     
 }
 
-extension PaymentVC: SSRadioButtonControllerDelegate {
-    func didSelectButton(selectedButton: UIButton?) {
-        
-        switch selectedButton {
+extension PaymentVC: SSRadioButtonControllerDelegate
+{
+    func didSelectButton(selectedButton: UIButton?)
+    {
+        switch selectedButton
+        {
         case codBtn:
             print("CoD")
+            if shouldPay! <= 10000
+            {
+                print("Approved")
+                processImage.image = UIImage(named: "money")
+                processText.text = "You will Pay by Cash"
+                approved = true
+
+            }
+            else
+            {
+                print("Denied")
+                processImage.image = UIImage(named: "cross")
+                processText.text = "We are sorry but the order is too huge to be paid in cash"
+                approved = false
+            }
+            
         case payPalBtn:
             print("PayPal")
+            processImage.image = UIImage(named: "paypal")
+            processText.text = "You will Pay by your PayPal Account"
+            approved = true
+
         default:
             print("NoBtn")
         }
