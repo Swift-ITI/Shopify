3c7c61d47347ac85f0187d8fc5a80c7d431b50ec
Michael <michaelhany101@gmail.com>
2023-03-10 00:27:29 +0200

    finish setting part completely and waits more features

--- Shopify/Views/ProfileScreen/AddressVC.swift
+++ Shopify/Views/ProfileScreen/AddressVC.swift
@@ -39,11 +39,11 @@ class AddressVC: UIViewController
 
     var userID : Int?
     var userDetails : Customers?
-    var arrOfAddresses : AddressesResult?
+    var arrOfAddresses : [Address] = []
     var userVM : UserViewModel?
     var deleteVM : PostAddressViewModel?
     var editVM : PostAddressViewModel?
-    
+    var refresh = UIRefreshControl()
     override func viewDidLoad()
     {
         super.viewDidLoad()
@@ -58,6 +58,10 @@ class AddressVC: UIViewController
         userVM?.bindAddressToVC = { () in self.renderAddressView()
             self.addressesTable.reloadData()}
         addressesTable.reloadData()
+        
+        refresh.tintColor = UIColor.black
+        refresh.addTarget(self, action: #selector(handleRefresh(refreshed: )), for: UIControl.Event.valueChanged)
+        addressesTable.addSubview(refresh)
     }
 
     override func viewWillAppear(_ animated: Bool)
@@ -67,6 +71,9 @@ class AddressVC: UIViewController
         userVM?.bindAddressToVC = { () in self.renderAddressView()
             self.addressesTable.reloadData()
         }
+        refresh.tintColor = UIColor.black
+        refresh.addTarget(self, action: #selector(handleRefresh(refreshed: )), for: UIControl.Event.valueChanged)
+        addressesTable.addSubview(refresh)
         //addressesTable.reloadData()
         //self.renderAddressView()
     }
@@ -84,7 +91,7 @@ class AddressVC: UIViewController
     {
         DispatchQueue.main.async
         {
-            self.arrOfAddresses = self.userVM?.addresses
+            self.arrOfAddresses = self.userVM?.addresses?.addresses ?? []
             self.addressesTable.reloadData()
         }
         //addressesTable.reloadData()
@@ -110,6 +117,23 @@ class AddressVC: UIViewController
         present(alert, animated: true, completion: nil)
     }
     
+    @objc func handleRefresh(refreshed:UIRefreshControl)
+    {
+        DispatchQueue.global().async
+        {
+            sleep(1)
+            DispatchQueue.main.async
+            {
+                self.userVM?.fetchAddresses(target: .searchCustomerAddresses(id: self.userID ?? 6810321223958))
+                self.userVM?.bindAddressToVC = { () in self.renderAddressView()
+                    self.addressesTable.reloadData()}
+                self.renderAddressView()
+                self.addressesTable.reloadData()
+                refreshed.endRefreshing()
+            }
+        }
+    }
+    
 // MARK: - IBActions Part
 
     @IBAction func addNewAddressButtonAction(_ sender: Any)
@@ -133,19 +157,19 @@ extension AddressVC : UITableViewDelegate, UITableViewDataSource
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int
     {
-        return arrOfAddresses?.addresses?.count ?? 1 //userDetails?.customers.first?.addresses?.count ?? 0
+        return arrOfAddresses.count //userDetails?.customers.first?.addresses?.count ?? 0
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell
     {
         let cell : AddressesCell = tableView.dequeueReusableCell(withIdentifier: "addressesCell", for: indexPath) as! AddressesCell
         
-        cell.cityLabel.text = "\(arrOfAddresses?.addresses?[indexPath.row].city ?? "No City"), \(arrOfAddresses?.addresses?[indexPath.row].address1 ?? "No Address")"
-        cell.phoneNumberLabel.text = arrOfAddresses?.addresses?[indexPath.row].phone
-        cell.countryLabel.text = arrOfAddresses?.addresses?[indexPath.row].country
+        cell.cityLabel.text = "\(arrOfAddresses[indexPath.row].city ?? "No City"), \(arrOfAddresses[indexPath.row].address1 ?? "No Address")"
+        cell.phoneNumberLabel.text = arrOfAddresses[indexPath.row].phone
+        cell.countryLabel.text = arrOfAddresses[indexPath.row].country
         cell.phoneNumberLabel.adjustsFontSizeToFitWidth = true
         
-        if arrOfAddresses?.addresses?[indexPath.row].default == true
+        if arrOfAddresses[indexPath.row].default == true
         {
             cell.checkMarkImage.image = UIImage(systemName: "checkmark.circle.fill")
         }else{
@@ -159,16 +183,14 @@ extension AddressVC : UITableViewDelegate, UITableViewDataSource
     {
         self.alertButtons(title: "Select Address", buttonTitle: "Select", message: "Are you sure that you want to make this your Default address", handler: {action in
             print("select address")
-            if self.arrOfAddresses?.addresses?[indexPath.row].default == true
-            {
-                self.changeDefault(customerID: self.userDetails?.customers.first?.id ?? 6810321223958, addressID: self.arrOfAddresses?.addresses?[indexPath.row].id ?? 0, defaultState: true)
-            }
-            
-            else if self.arrOfAddresses?.addresses?[indexPath.row].default == false
-            {
-                self.changeDefault(customerID: self.userDetails?.customers.first?.id ?? 6810321223958, addressID: self.arrOfAddresses?.addresses?[indexPath.row].id ?? 0, defaultState: false)
-            }
+            self.changeDefault(customerID: self.userDetails?.customers.first?.id ?? 6810321223958, addressID: self.arrOfAddresses[indexPath.row].id ?? 0, defaultState: self.arrOfAddresses[indexPath.row].default ?? true)
+            self.userVM?.fetchAddresses(target: .searchCustomerAddresses(id: self.userID ?? 6810321223958))
+            self.userVM?.bindAddressToVC = { () in self.renderAddressView()
+                self.addressesTable.reloadData()}
             self.renderAddressView()
+            self.refresh.tintColor = UIColor.black
+            self.refresh.addTarget(self, action: #selector(self.handleRefresh(refreshed: )), for: UIControl.Event.valueChanged)
+            self.addressesTable.addSubview(self.refresh)
         })
     }
     
@@ -176,10 +198,62 @@ extension AddressVC : UITableViewDelegate, UITableViewDataSource
     {
         return true
     }
-        
-     func tableView(_ tableView: UITableView, editActionsForRowAt indexPath: IndexPath) -> [UITableViewRowAction]?
+    
+    func tableView(_ tableView: UITableView, leadingSwipeActionsConfigurationForRowAt indexPath: IndexPath) -> UISwipeActionsConfiguration?
     {
-// Edit
+        let editAction = UIContextualAction(style: .normal, title: "Edit", handler: {
+            action, view, completion in
+            completion(true)
+            print("Edit")
+            self.alertButtons(title: "Edit Address", buttonTitle: "Edit", message: "Are you sure that you want to Edit this saved address!", handler: {action in
+                print("edit address")
+                let addAddressView = self.storyboard?.instantiateViewController(withIdentifier: "addaddressVC") as! AddAddressVC
+                addAddressView.userID = self.userID ?? 6810321223958
+                addAddressView.flag = 2
+                addAddressView.address = self.arrOfAddresses[indexPath.row]
+                
+                self.navigationController?.pushViewController(addAddressView, animated: true)
+            })
+        })
+        editAction.image = UIImage(systemName: "pencil")
+        editAction.backgroundColor = UIColor .blue
+        let configuration = UISwipeActionsConfiguration(actions: [editAction])
+        return configuration
+    }
+    
+    func tableView(_ tableView: UITableView, trailingSwipeActionsConfigurationForRowAt indexPath: IndexPath) -> UISwipeActionsConfiguration?
+    {
+        let deleteAction = UIContextualAction(style: .normal, title: "Delete", handler: {
+            action, view, completion in
+            completion(true)
+            print("Delete")
+            if self.arrOfAddresses[indexPath.row].default == false
+            {
+                self.alertButtons(title: "Delete Address", buttonTitle: "Delete", message: "Are you sure that you want to Delete this saved address!", handler: {action in
+                    self.deleteVM?.deleteAddress(target: .deleteAddress(customerID: self.userDetails?.customers.first?.id ?? 6810321223958, addressID: self.arrOfAddresses[indexPath.row].id ?? 9050959642902))
+                    print("delete address")
+                    self.arrOfAddresses.remove(at: indexPath.row)
+                    tableView.deleteRows(at: [indexPath], with: .fade)
+                    tableView.reloadData()
+                })
+            }
+            else if self.arrOfAddresses[indexPath.row].default == true
+            {
+                //self.alertButtons(title: "Can't Delete", buttonTitle: "Cancel", message: "Sorry you can't delete your default address", handler: nil)
+                let alert = UIAlertController(title: "Delete Unavailable", message: "Sorry you can't delete your default address", preferredStyle: UIAlertController.Style.alert)
+                alert.addAction(UIAlertAction(title: "Cancel", style: .cancel))
+                self.present(alert, animated: true, completion: nil)
+            }
+        })
+        deleteAction.image = UIImage(systemName: "trash")
+        deleteAction.backgroundColor = UIColor .red
+        let configuration = UISwipeActionsConfiguration(actions: [deleteAction])
+        return configuration
+    }
+     
+    /*func tableView(_ tableView: UITableView, editActionsForRowAt indexPath: IndexPath) -> [UITableViewRowAction]?
+    {
+        // Edit
         let editButton = UITableViewRowAction(style: .default, title: "Edit", handler: {(rowAction, indexPath) in
             print("Edit")
             self.alertButtons(title: "Edit Address", buttonTitle: "Edit", message: "Are you sure that you want to Edit this saved address!", handler: {action in
@@ -187,28 +261,28 @@ extension AddressVC : UITableViewDelegate, UITableViewDataSource
                 let addAddressView = self.storyboard?.instantiateViewController(withIdentifier: "addaddressVC") as! AddAddressVC
                 addAddressView.userID = self.userID ?? 6810321223958
                 addAddressView.flag = 2
-                addAddressView.address = self.arrOfAddresses?.addresses?[indexPath.row]
+                addAddressView.address = self.arrOfAddresses[indexPath.row]
                 
                 self.navigationController?.pushViewController(addAddressView, animated: true)
             })
         })
         editButton.backgroundColor = UIColor.systemBlue
         
-// Delete
+        // Delete
         let deleteButton = UITableViewRowAction(style: .default, title: "Delete", handler: {(rowAction, indexPath) in
             print("Delete")
-            if self.arrOfAddresses?.addresses?[indexPath.row].default == false
+            if self.arrOfAddresses[indexPath.row].default == false
             {
                 self.alertButtons(title: "Delete Address", buttonTitle: "Delete", message: "Are you sure that you want to Delete this saved address!", handler: {action in
-                    self.deleteVM?.deleteAddress(target: .deleteAddress(customerID: self.userDetails?.customers.first?.id ?? 6810321223958, addressID: self.arrOfAddresses?.addresses?[indexPath.row].id ?? 9050959642902))
+                    self.deleteVM?.deleteAddress(target: .deleteAddress(customerID: self.userDetails?.customers.first?.id ?? 6810321223958, addressID: self.arrOfAddresses[indexPath.row].id ?? 9050959642902))
                     print("delete address")
-                    self.arrOfAddresses?.addresses?.remove(at: indexPath.row)
+                    self.arrOfAddresses.remove(at: indexPath.row)
                     tableView.deleteRows(at: [indexPath], with: .fade)
                     tableView.reloadData()
                 })
             }
             
-            else if self.arrOfAddresses?.addresses?[indexPath.row].default == true
+            else if self.arrOfAddresses[indexPath.row].default == true
             {
                 //self.alertButtons(title: "Can't Delete", buttonTitle: "Cancel", message: "Sorry you can't delete your default address", handler: nil)
                 let alert = UIAlertController(title: "Delete Unavailable", message: "Sorry you can't delete your default address", preferredStyle: UIAlertController.Style.alert)
@@ -218,5 +292,5 @@ extension AddressVC : UITableViewDelegate, UITableViewDataSource
         })
         deleteButton.backgroundColor = UIColor.systemRed
         return [editButton, deleteButton]
-    }
+    }*/
 }
